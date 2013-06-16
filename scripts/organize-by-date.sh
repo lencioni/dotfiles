e725b1a75c620ac10f03ccefa72e536719d863e4
Joe Lencioni <joe.lencioni@causes.com>
2013-06-16 15:15:34 -0700

    Make file organization script work with spaces
    The previous iteration of the script didn't work if the filename
    contained spaces.
    
    Solution taken from:
    
      http://unix.stackexchange.com/a/9499

--- scripts/organize-by-date.sh
+++ scripts/organize-by-date.sh
@@ -4,13 +4,13 @@
 # and sorts them into subdirectories according to the date
 # Creates subdirectories corresponding to the dates as necessary.
 
-for file in $(find . -type f)
-do
-    datepath="$(stat -f %c $file | xargs -J @ date -r @ +'%Y/%m')"
-    if ! test -e "$datepath"; then
-        mkdir -pv "$datepath"
-    fi
+find . -type f -exec sh -c '
+  file="$0"
+  echo $file
+  datepath=`stat -f %c "$file" | xargs -J @ date -r @ +"%Y/%m"`
+  if ! test -e "$datepath"; then
+      mkdir -pv "$datepath"
+  fi
 
-    mv -v $file $datepath
-done
-
+  mv -v "$file" ./$datepath
+' {} ';'
