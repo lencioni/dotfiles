3c784d4cdd9a8f75d3709e971093c40c7d53b1e8
Joe Lencioni <joe.lencioni@causes.com>
2013-06-16 12:28:12 -0700

    Add organize-by-date.sh
    This script will organize the files in a directory into nested year and
    month directories, based on the ctimes of the files. I intend to use
    this to organize photos after importing them from my camera.

---
+++ scripts/organize-by-date.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Goes through all jpeg files in current directory, grabs date from each
+# and sorts them into subdirectories according to the date
+# Creates subdirectories corresponding to the dates as necessary.
+
+for file in *
+do
+    datepath="$(stat -f %c $file | xargs -J @ date -r @ +'%Y/%m')"
+    if ! test -e "$datepath"; then
+        mkdir -pv "$datepath"
+    fi
+
+    mv -v $file $datepath
+done
+
