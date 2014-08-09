3469adfe6c6c8534a41602562726025467bc9aae
Joe Lencioni <joe.lencioni@brigade.com>
2014-08-09 13:55:23 -0700

    Fix typo in update script
    I had mistyped "submodule". Git was smart enough to figure out what I
    meant anyway, but it is nice to set things right.

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,7 +1,7 @@
 git submodule foreach git checkout master
 git submodule foreach git fetch --all
 git submodule foreach git reset --hard origin/master
-git submoudle update --init --recursive
+git submodule update --init --recursive
 
 cd .vim/bundle/command-t/ruby/command-t
 ruby extconf.rb
