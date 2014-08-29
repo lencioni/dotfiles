76108d191cbee343e34d58b17233f6ed7eea7f23
Joe Lencioni <joe.lencioni@brigade.com>
2014-08-28 22:14:23 -0700

    Vim: improve update script
    My update script wasn't quite doing what I wanted, because of the order
    of the commands it was executing, so I reconfigured it a bit.
    
    This was mostly borrowed from @wincent's update script [1]. 3 key
    differences:
    
      1. I made it run YouCompleteMe's install script every time it is run.
         It only takes a minute or two to install, and I don't run the
         update script very often, so I don't mind paying this penalty every
         time since it is likely what I want to happen anyway.
      2. I made it install Command-T every time. Similar to YouCompleteMe
         above.
      3. I left out the rev-pinning. I like to live on the edge, which means
         that I am always using latest master for my Vim plugins. I decided
         that I'll roll without this feature until I actually have a need
         for it.
    
    [1]: https://github.com/wincent/wincent/blob/master/update-bundle.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,10 +1,19 @@
-git submodule foreach git checkout master
-git submodule foreach git fetch --all
-git submodule foreach git reset --hard origin/master
-git submodule update --init --recursive
+set -e
 
+git submodule foreach git checkout master
+git submodule foreach git pull --recurse-submodules
+
+# Install YouCompleteMe
+cd .vim/bundle/YouCompleteMe
+git submodule update --init --recursive
+./install.sh
+cd -
+
+# Install Command-T
 cd .vim/bundle/command-t/ruby/command-t
 ruby extconf.rb
 make clean
 make
 cd -
+
+vim -c "call pathogen#helptags() | quit"
