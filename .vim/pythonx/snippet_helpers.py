e9966c9a1925f82bbfcc85f344699a9ebbccaffc
Joe Lencioni <joe.lencioni@brigade.com>
2014-12-02 10:09:15 -0800

    Vim: rename pascalCase snippet helper to pascal_case_basename
    I am about to add another function here and realized that the name I had
    originally given to this one wasn't as good as I like it to be. Also, I
    realized that the naming convention in Python is snake_case, not
    camelCase, so I made that adjustment at the same time.

--- .vim/pythonx/snippet_helpers.py
+++ .vim/pythonx/snippet_helpers.py
@@ -1,5 +1,5 @@
 import re
 
-def pascalCase(basename):
+def pascal_case_basename(basename):
     cleaned_name = re.sub('\.js$', '', basename or 'ModuleName')
     return ''.join(x.title() for x in cleaned_name.split('_'))
