3d26f41c2d97447f3f43b81e3dfa4e8e045efce6
Joe Lencioni <joe.lencioni@brigade.com>
2014-12-02 10:25:15 -0800

    Vim: add snippet for Jasmine subject pattern
    As @trotzig wrote about on the Brigade engineering blog [1], we are
    using a `this.subject` pattern in our Jasmine specs. This snippet makes
    working with this pattern a little nicer.
    
    [1]: https://medium.com/brigade-engineering/dry-up-your-jasmine-specs-with-a-subject-d941b888db97

--- .vim/pythonx/snippet_helpers.py
+++ .vim/pythonx/snippet_helpers.py
@@ -1,7 +1,7 @@
 import re
 
 def pascal_case_basename(basename):
-    cleaned_name = re.sub('\.js$', '', basename or 'ModuleName')
+    cleaned_name = re.sub('(_spec)?(\.js$)?', '', basename or 'ModuleName')
     return ''.join(x.title() for x in cleaned_name.split('_'))
 
 def dasherize_basename(basename):
