57dd5893028e5885b30f1dd1f059e2f43d0bc0a0
Joe Lencioni <joe.lencioni@brigade.com>
2014-12-02 10:19:40 -0800

    Vim: add BEM css snippet
    We are using the BEM methodology and breaking the styles for our
    components down into separate files that match the BEM block name. I can
    leverage the power of UltiSnips here to make my work more efficient.

--- .vim/pythonx/snippet_helpers.py
+++ .vim/pythonx/snippet_helpers.py
@@ -3,3 +3,7 @@ import re
 def pascal_case_basename(basename):
     cleaned_name = re.sub('\.js$', '', basename or 'ModuleName')
     return ''.join(x.title() for x in cleaned_name.split('_'))
+
+def dasherize_basename(basename):
+  cleaned_name = re.sub('\.css$', '', basename or 'module-name')
+  return '-'.join(filter(None, cleaned_name.split('_')))
