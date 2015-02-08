231ba0735294e9ab08e4b782a60a466e876607a4
Joe Lencioni <joe.lencioni@brigade.com>
2015-02-08 07:55:44 -0800

    Vim: add some JSX propTypes snippets
    I've been using these to make me a little more productive when writing
    React components.
    
    I got pretty fancy with one by adding some autocompletion to it, and
    I've been on the fence about it, but I figured I'd put it in here for
    now and see how it goes.
    
    While I was at it, I fixed a number of warnings that flake8 told me
    about in my Python code.

--- .vim/pythonx/snippet_helpers.py
+++ .vim/pythonx/snippet_helpers.py
@@ -1,9 +1,19 @@
 import re
 
+
 def pascal_case_basename(basename):
     cleaned_name = re.sub('(_spec)?(\.js$)?', '', basename or 'ModuleName')
     return ''.join(x.title() for x in cleaned_name.split('_'))
 
+
 def dasherize_basename(basename):
-  cleaned_name = re.sub('\.css$', '', basename or 'module-name')
-  return '-'.join(filter(None, cleaned_name.split('_')))
+    cleaned_name = re.sub('\.css$', '', basename or 'module-name')
+    return '-'.join(filter(None, cleaned_name.split('_')))
+
+
+def complete(text, opts):
+    if text:
+        opts = [m[len(text):] for m in opts if m.startswith(text)]
+    if len(opts) == 1:
+        return opts[0]
+    return '(' + '|'.join(opts) + ')'
