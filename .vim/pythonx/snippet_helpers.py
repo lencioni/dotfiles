162fd2cc63261497a65f89e20894126913ff45e0
Joe Lencioni <joe.lencioni@brigade.com>
2015-07-13 18:14:54 -0500

    Vim: update snippets for component-centric structure
    At Brigade, we used to organize our components like:
    
      /app/assets/javascripts/components/my_component_name.jsx
      /app/assets/stylesheets/components/_my_component_name.scss
      /spec/javascripts/components/my_component_name_spec.jsx
    
    However, we have recently decided to start organizing them in a
    different way:
    
      /app/assets/components/MyComponentName/index.jsx
      /app/assets/components/MyComponentName/index.scss
      /app/assets/components/MyComponentName/index-test.jsx
    
    This commit updates my snippets to work with both styles.

--- .vim/pythonx/snippet_helpers.py
+++ .vim/pythonx/snippet_helpers.py
@@ -1,14 +1,37 @@
+import os
 import re
 
 
 def pascal_case_basename(basename):
-    cleaned_name = re.sub('(_spec)?(\.js$)?', '', basename or 'ModuleName')
-    return ''.join(x.title() for x in cleaned_name.split('_'))
+    return ''.join(x[0].upper() + x[1:] for x in basename.split('_'))
 
 
 def dasherize_basename(basename):
-    cleaned_name = re.sub('\.css$', '', basename or 'module-name')
-    return '-'.join(filter(None, cleaned_name.split('_')))
+    return '-'.join(filter(None, _convert_camel_case(basename).split('_')))
+
+
+# http://stackoverflow.com/a/1176023/18986
+def _convert_camel_case(string):
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1-\2', string)
+    return re.sub('([a-z0-9])([A-Z])', r'\1-\2', s1).lower()
+
+
+def _clean_basename(basename):
+    return re.sub('(_spec|-test)$', '', basename or 'ModuleName')
+
+
+# If path ends in `index.jsx`, this function will return the PascalCased
+# directory name. Otherwise, it returns the PascalCased filename. This allows
+# me to use my snippets with modules that are like `/path/to/module_name.jsx`
+# and modules that are like `/path/to/ModuleName/index.jsx`.
+def path_to_component_name(path, case_fn):
+    dirname, filename = os.path.split(path)
+    basename = os.path.splitext(filename)[0]
+    if basename == 'index' or basename == 'index-test':
+        # Pop the last directory name off the dirname
+        return case_fn(_clean_basename(os.path.basename(dirname)))
+    else:
+        return case_fn(_clean_basename(basename))
 
 
 def complete(text, opts):
