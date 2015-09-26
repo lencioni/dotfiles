a026a5c9a1561e9ab8b512db089aadd3d35a89d2
Joe Lencioni <joe.lencioni@brigade.com>
2015-09-26 06:02:23 -0700

    Scripts: add useful output to update script
    This will give me some waypoints to help with debugging if I need it.

--- scripts/update.sh
+++ scripts/update.sh
@@ -13,11 +13,13 @@ cd -
 # Install YouCompleteMe
 cd .vim/bundle/YouCompleteMe
 git submodule update --init --recursive
+echo 'Installing YouCompleteMe...'
 ./install.sh
 cd -
 
 # Install Command-T
 cd .vim/bundle/command-t/ruby/command-t
+echo 'Building Command-T...'
 ruby extconf.rb
 make clean
 make
