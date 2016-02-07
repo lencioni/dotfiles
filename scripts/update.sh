2b5e5bd489da95d309c52a48d74b358345c30364
Joe Lencioni <joe.lencioni@gmail.com>
2016-02-07 08:13:29 -0800

    Scripts: use subshells instead of `cd -`
    Spawning subshells here probably won't make this any faster, but it
    seems safer than my previous method.

--- scripts/update.sh
+++ scripts/update.sh
@@ -6,23 +6,28 @@ git submodule foreach git checkout master
 git submodule foreach git pull --recurse-submodules
 
 # Update prezto
-cd .zprezto
-git submodule update --init --recursive
-cd -
+(
+  cd .zprezto
+  git submodule update --init --recursive
+)
 
 # Install YouCompleteMe
-cd .vim/bundle/YouCompleteMe
-git submodule update --init --recursive
-echo 'Installing YouCompleteMe...'
-./install.py
-cd -
+(
+  cd .vim/bundle/YouCompleteMe
+  git submodule update --init --recursive
+  echo 'Installing YouCompleteMe...'
+  ./install.py
+)
 
 # Install Command-T
-cd .vim/bundle/command-t/ruby/command-t
-echo 'Building Command-T...'
-ruby extconf.rb
-make clean
-make
-cd -
+(
+  cd .vim/bundle/command-t/ruby/command-t
+  echo 'Building Command-T...'
+  ruby extconf.rb
+  make clean
+  make
+)
+
+wait
 
 vim -c "call pathogen#helptags() | quit"
