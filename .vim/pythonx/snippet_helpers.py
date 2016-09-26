029c37ca98ce0f3aa76b0d4e744b5d4778705f43
Joe Lencioni <joe.lencioni@airbnb.com>
2016-09-26 08:52:37 -0700

    Vim: use ''' for multi-line Python comment
    This is the correct style to use in this language.

--- .vim/pythonx/snippet_helpers.py
+++ .vim/pythonx/snippet_helpers.py
@@ -20,10 +20,12 @@ def _clean_basename(basename):
     return re.sub('(_spec|-test|-diffux)$', '', basename or 'ModuleName')
 
 
-# If path ends in `index.jsx`, this function will return the PascalCased
-# directory name. Otherwise, it returns the PascalCased filename. This allows
-# me to use my snippets with modules that are like `/path/to/module_name.jsx`
-# and modules that are like `/path/to/ModuleName/index.jsx`.
+'''
+If path ends in `index.jsx`, this function will return the PascalCased directory
+name. Otherwise, it returns the PascalCased filename. This allows me to use my
+snippets with modules that are like `/path/to/module_name.jsx` and modules that
+are like `/path/to/ModuleName/index.jsx`.
+'''
 def path_to_component_name(path, case_fn):
     dirname, filename = os.path.split(path)
     basename = os.path.splitext(filename)[0]
