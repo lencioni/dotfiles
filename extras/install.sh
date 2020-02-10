137eac7c72935acc3328609949a79bd84a3cacda
Joe Lencioni <joe.lencioni@gmail.com>
2020-02-10 11:14:04 -0600

    tmux: Simplify terminfo install script
    While I was setting up a new laptop, this install script didn't do its
    job. Simplifying it and fixing up some paths should help it do the right
    thing next time and also make it easier to debug.

--- extras/install.sh
+++ extras/install.sh
@@ -2,28 +2,15 @@
 
 set -e
 
-# usually this means running on a machine with a statically-linked, hand-built
-# tmux (and ncurses)
-if [ -d "$HOME/share/terminfo" ]; then
-  export TERMINFO=$HOME/share/terminfo
-else
-  export TERMINFO=$HOME/.terminfo
-fi
+export TERMINFO=$HOME/.terminfo
 
 mkdir -p "$TERMINFO"
-mkdir -p dry-run
 
 patch_terminfo() {
-  T=$1
-  infocmp "$T" > "$T.terminfo.original" || true
-  tic -o dry-run "$T.terminfo"
-  infocmp -A dry-run "$T" > "$T.terminfo.new"
-  echo ----- "$T" -----
-  diff -u "$T.terminfo.original" "$T.terminfo.new" || true
-  echo ----- "$T" -----
-  tic "$T.terminfo"
+  FILENAME=$1
+  tic -o "$TERMINFO" "$HOME/dotfiles/extras/$FILENAME"
 }
 
-patch_terminfo tmux
-patch_terminfo tmux-256color
-patch_terminfo xterm-256color
+patch_terminfo tmux.terminfo
+patch_terminfo tmux-256color.terminfo
+patch_terminfo xterm-256color.terminfo
