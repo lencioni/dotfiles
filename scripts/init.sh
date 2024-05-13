cd16904794141bd99268012a6da7d92f2671bb86
Joe Lencioni <joe.lencioni@gmail.com>
2024-05-13 09:23:36 -0500

    scripts: Make symlinks in a subshell in init.sh
    I am changing the directory here to run a bunch of commands. I want to
    encapsulate this so it does not impact the directory for other commands
    or for after the init script is done running, and we can accomplish this
    by using a subshell.

--- scripts/init.sh
+++ scripts/init.sh
@@ -7,30 +7,33 @@ set -e
 chsh -s /bin/zsh
 
 git submodule update --init --recursive
-cd ~
-ln -sFi dotfiles/.ackrc .
-ln -sFi dotfiles/.agignore .
-ln -sFi dotfiles/.ctags .
-ln -sFi dotfiles/.gemrc .
-ln -sFi dotfiles/.gitattributes .
-ln -sFi dotfiles/.gitconfig .
-ln -sFi dotfiles/.gitexcludes .
-ln -sFi dotfiles/.hammerspoon .
-ln -sFi dotfiles/.p10k.zsh .
-ln -sFi dotfiles/.rdebugrc .
-ln -sFi dotfiles/.rspec .
-ln -sFi dotfiles/.tmux.conf .
-ln -sFi dotfiles/.vimrc .
-ln -sFi dotfiles/.vim .
-ln -sFi dotfiles/.zsh .
 
-ln -sFi dotfiles/.zprezto .
-ln -sFi dotfiles/.zpreztorc .
-ln -sFi dotfiles/.zprofile .
-ln -sFi dotfiles/.zshrc .
-ln -sFi .zprezto/runcoms/zlogin .zlogin
-ln -sFi .zprezto/runcoms/zlogout .zlogout
-ln -sFi .zprezto/runcoms/zshenv .zshenv
+(
+  cd ~
+  ln -sFi dotfiles/.ackrc .
+  ln -sFi dotfiles/.agignore .
+  ln -sFi dotfiles/.ctags .
+  ln -sFi dotfiles/.gemrc .
+  ln -sFi dotfiles/.gitattributes .
+  ln -sFi dotfiles/.gitconfig .
+  ln -sFi dotfiles/.gitexcludes .
+  ln -sFi dotfiles/.hammerspoon .
+  ln -sFi dotfiles/.p10k.zsh .
+  ln -sFi dotfiles/.rdebugrc .
+  ln -sFi dotfiles/.rspec .
+  ln -sFi dotfiles/.tmux.conf .
+  ln -sFi dotfiles/.vimrc .
+  ln -sFi dotfiles/.vim .
+  ln -sFi dotfiles/.zsh .
+
+  ln -sFi dotfiles/.zprezto .
+  ln -sFi dotfiles/.zpreztorc .
+  ln -sFi dotfiles/.zprofile .
+  ln -sFi dotfiles/.zshrc .
+  ln -sFi .zprezto/runcoms/zlogin .zlogin
+  ln -sFi .zprezto/runcoms/zlogout .zlogout
+  ln -sFi .zprezto/runcoms/zshenv .zshenv
+)
 
 mkdir -p "$HOME/Library/Application\ Support/Code"
 ln -sFi "$HOME/dotfiles/vscode" "$HOME/Library/Application\ Support/Code/User"
